0569494723b734df2efd6d85c3f718e56df0c4ad
Bang Van <bangtranvan08@gmail.com>
2023-10-18 19:32:45 +0700

    update

--- src/test_code_in_java/Test_code.java
+++ src/test_code_in_java/Test_code.java
@@ -1,9 +1,18 @@
 package test_code_in_java;
 
 public class Test_code {
-    public static void main(String[] args)
-    {
-        System.out.println("LMao!!");
-        System.out.printf("Hello %d %s",18,"World!");
+
+  public static void main(String[] args) {
+    System.out.println("LMao!!");
+    System.out.printf("Hello %d %s", 18, "World!");
+    System.out.println();
+    String s = "Tran Van Bang";
+    String t = "Tran Van Bang";
+    boolean a = false;
+    if (s.equals(t)) {
+      a = true;
     }
+    System.out.println(a);
+    // System.out.println(s);
+  }
 }
