fa6f28b046f8fb910ca8bc7206ddfce3d97bcd9a
Bang Van <bangtranvan08@gmail.com>
2023-10-18 19:04:43 +0700

    Update

---
+++ src/TheFirstJavaProgram.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class TheFirstJavaProgram {
+
+  public static void main(String[] args) {
+    JFrame myWindow;
+    myWindow = new JFrame();
+    myWindow.setSize(300, 200);
+    myWindow.setTitle("My first Java program");
+    myWindow.setVisible(true);
+  }
+}
