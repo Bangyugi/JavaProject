4134b774e72dbea388e9eb512a8d4bc9a3f16e37
Bang Van <bangtranvan08@gmail.com>
2023-10-18 19:14:35 +0700

    update

--- src/buoi_1/Hello_world_in_java.java
+++ src/buoi_1/Hello_world_in_java.java
@@ -1,5 +1,8 @@
 package buoi_1;
 
 public class Hello_world_in_java {
-    public static void main
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world in java");
+    }
 }
