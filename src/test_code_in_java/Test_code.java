368756c2a13149b9eb0742bd580789955638b29f
Bang Van <bangtranvan08@gmail.com>
2023-10-18 19:25:09 +0700

    update

--- src/test_code_in_java/Test_code.java
+++ src/test_code_in_java/Test_code.java
@@ -1,8 +1,9 @@
-package buoi_2;
+package test_code_in_java;
 
 public class Test_code {
     public static void main(String[] args)
     {
         System.out.println("LMao!!");
+        System.out.printf("Hello %d %s",18,"World!");
     }
 }
