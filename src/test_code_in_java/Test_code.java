d70b33f3dcc46e34785d6bd94b741ea7489f1716
Bang Van <bangtranvan08@gmail.com>
2023-10-18 19:37:45 +0700

    Update

--- src/test_code_in_java/Test_code.java
+++ src/test_code_in_java/Test_code.java
@@ -1,18 +1,25 @@
 package test_code_in_java;
 
+import java.util.Scanner;
+
 public class Test_code {
 
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
     System.out.println("LMao!!");
     System.out.printf("Hello %d %s", 18, "World!");
     System.out.println();
     String s = "Tran Van Bang";
     String t = "Tran Van Bang";
-    boolean a = false;
+    boolean check = false;
     if (s.equals(t)) {
-      a = true;
+      check = true;
     }
-    System.out.println(a);
-    // System.out.println(s);
+    System.out.println(check);
+    int a = scan.nextInt();
+    String s1 = scan.next();
+    String s2 = scan.nextLine();
+    // nextLine nhập chuỗi trên 1 dòng còn next thì không (tức là nó sẽ nhập chuỗi có chứa khoảng trắng)
+    
   }
 }
